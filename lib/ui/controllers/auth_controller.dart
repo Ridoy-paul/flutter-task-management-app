cac54e29fc47730053153e680a8d75718a053e68
Ridoy paul <cse.ridoypaul@gmail.com>
2023-11-19 11:14:16 +0600

    User model updated

--- lib/ui/controllers/auth_controller.dart
+++ lib/ui/controllers/auth_controller.dart
@@ -1,3 +1,22 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../data/models/user_model.dart';
+
 class AuthController {
+  static String? token;
+
+  Future<void> saveUserInformation(String token, UserModel model) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString('token', token);
+    await sharedPreferences.setString('user', jsonEncode(model.toJson()));
+  }
+
+  Future<void> initilizeUserCache() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    token = sharedPreferences.getString('token');
+
+  }
 
 }
