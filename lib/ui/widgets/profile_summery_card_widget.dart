16023df4a1471a7fffe7278dfb637cc61c38adda
Ridoy paul <cse.ridoypaul@gmail.com>
2023-11-20 00:34:11 +0600

    updated

--- lib/ui/widgets/profile_summery_card_widget.dart
+++ lib/ui/widgets/profile_summery_card_widget.dart
@@ -4,7 +4,7 @@ import '../screens/edit_profile_screen.dart';
 import '../screens/login_screen.dart';
 import '../style.dart';
 
-class ProfileSummery extends StatelessWidget {
+class ProfileSummery extends StatefulWidget {
   const ProfileSummery({
     super.key,
     this.enableOnTap = true,
@@ -13,22 +13,32 @@ class ProfileSummery extends StatelessWidget {
   final bool enableOnTap;
 
   @override
+  State<ProfileSummery> createState() => _ProfileSummeryState();
+}
+
+class _ProfileSummeryState extends State<ProfileSummery> {
+  @override
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {
-        if(enableOnTap) {
+        if(widget.enableOnTap) {
           Navigator.push(context, MaterialPageRoute(builder: (context) => const EditProfileScreen(),),);
         }
       },
       leading: const CircleAvatar(
         child: Icon(Icons.person),
       ),
-      title: const Text("Ridoy Paul", style: TextStyle(color: colorWhite, fontWeight: FontWeight.w700),),
-      subtitle: const Text("cse.ridoypaul@gmail.com", style: TextStyle(color: colorWhite),),
+      title: Text(
+        fullName,
+        style: const TextStyle(color: colorWhite, fontWeight: FontWeight.w700),
+      ),
+      subtitle: Text(AuthController.user?.email ?? '', style: const TextStyle(color: colorWhite),),
       trailing: IconButton(
         onPressed: () async{
           await AuthController.clearAuthData();
-          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginScreen()), (route) => false);
+          if(mounted) {
+            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginScreen()), (route) => false);
+          }
         },
         color: colorWhite,
         icon: const Icon(Icons.logout),
@@ -36,4 +46,10 @@ class ProfileSummery extends StatelessWidget {
       tileColor: colorGreen,
     );
   }
+
+  // Get the full name from AuthController
+  String get fullName {
+    return '${AuthController.user?.firstName ?? ''} ${AuthController.user?.lastName ?? ''}';
+  }
+
 }
--- lib/ui/widgets/task_item_card_widget.dart
+++ lib/ui/widgets/task_item_card_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../style.dart';
 
 class TaskItemCard extends StatelessWidget {
