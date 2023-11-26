ea2efb72be6308857731b325482bbaab72764c40
Ridoy paul <cse.ridoypaul@gmail.com>
2023-11-26 22:25:55 +0600

    updated new

--- lib/ui/screens/add_new_task_screen.dart
+++ lib/ui/screens/add_new_task_screen.dart
@@ -14,7 +14,7 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
 
   final TextEditingController _titleTEController = TextEditingController();
   final TextEditingController _descriptionTEController = TextEditingController();
-  final GlobalKey<FormState> _addNewTaskKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _addNewTaskFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,44 +27,47 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
               child: BodyBackgroundWidget(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      Text(
-                          "Add New Task",
-                          style: Theme.of(context).textTheme.titleLarge),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: inputStyle("Title"),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormField(
-                        maxLines: 8,
-                        decoration: const InputDecoration(
-                          hintText: "Decoration"
+                  child: Form(
+                    key: _addNewTaskFormKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 40,
                         ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            //Navigator.push(context, MaterialPageRoute(builder: (context) => const MainBottomNavScreen(),),);
-                          },
-                          child: const Icon(Icons.arrow_circle_right_outlined),
+                        Text(
+                            "Add New Task",
+                            style: Theme.of(context).textTheme.titleLarge),
+                        const SizedBox(
+                          height: 16,
                         ),
-                      ),
-                    ],
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: inputStyle("Title"),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          maxLines: 8,
+                          decoration: const InputDecoration(
+                            hintText: "Decoration"
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              //Navigator.push(context, MaterialPageRoute(builder: (context) => const MainBottomNavScreen(),),);
+                            },
+                            child: const Icon(Icons.arrow_circle_right_outlined),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
